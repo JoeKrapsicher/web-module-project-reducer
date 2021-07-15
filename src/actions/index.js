f5d3b2a5ffb8e817d78c07b237b13da13a60af8a
JoeKrapsicher <JoeKrapsicher@gmail.com>
2021-07-15 16:23:20 -0600

    Completed Assignment

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,15 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+
+export const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY})
+}
+
+export const changeOperation = (opperator) => {
+    return({type:CHANGE_OPERATION, payload:opperator})
+}
 
 export const addOne = () => {
     return({type:ADD_ONE});
